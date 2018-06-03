4a0b5ad5e93ab0f4e053cb1d5c2a45e166c18dc3
cocafe <cocafehj@gmail.com>
2018-06-03 12:04:33 +0800

    src: add winring0 loader
    Signed-off-by: cocafe <cocafehj@gmail.com>

---
+++ include/winring0.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "../lib/WinRing0/OlsDef.h"
+#include "../lib/WinRing0/OlsApi.h"
+
+int WinRing0_init(void);
+void WinRing0_deinit(void);
---
+++ src/winring0.cpp
@@ -0,0 +1,79 @@
+#include <Windows.h>
+
+#include "winring0.h"
+
+#define lpLoaderName L"WinRing0 Loader"
+
+#pragma comment(lib, "../lib/WinRing0x64.lib")
+
+int WinRing0_init(void)
+{
+	if (!InitializeOls()) {
+		MessageBox(NULL,
+			   L"Failed to load DLL",
+			   lpLoaderName,
+			   MB_ICONERROR | MB_OK);
+		return -EFAULT;
+	}
+
+	switch (GetDllStatus())
+	{
+		case OLS_DLL_NO_ERROR:
+			break;
+
+		case OLS_DLL_UNSUPPORTED_PLATFORM:
+			MessageBox(NULL,
+				   L"DLL Status Error!! UNSUPPORTED_PLATFORM",
+				   lpLoaderName,
+				   MB_ICONERROR | MB_OK);
+			goto dll_deinit;
+
+		case OLS_DLL_DRIVER_NOT_LOADED:
+			MessageBox(NULL,
+				   L"DLL Status Error!! DRIVER_NOT_LOADED",
+				   lpLoaderName,
+				   MB_ICONERROR | MB_OK);
+			goto dll_deinit;
+
+		case OLS_DLL_DRIVER_NOT_FOUND:
+			MessageBox(NULL,
+				   L"DLL Status Error!! DRIVER_NOT_FOUND",
+				   lpLoaderName,
+				   MB_ICONERROR | MB_OK);
+			goto dll_deinit;
+
+		case OLS_DLL_DRIVER_UNLOADED:
+			MessageBox(NULL,
+				   L"DLL Status Error!! DRIVER_UNLOADED",
+				   lpLoaderName,
+				   MB_ICONERROR | MB_OK);
+			goto dll_deinit;
+
+		case OLS_DLL_DRIVER_NOT_LOADED_ON_NETWORK:
+			MessageBox(NULL,
+				   L"DLL Status Error!! DRIVER_NOT_LOADED_ON_NETWORK",
+				   lpLoaderName,
+				   MB_ICONERROR | MB_OK);
+			goto dll_deinit;
+
+		case OLS_DLL_UNKNOWN_ERROR:
+		default:
+			MessageBox(NULL,
+				   L"DLL Status Error!! UNKNOWN_ERROR",
+				   lpLoaderName,
+				   MB_ICONERROR | MB_OK);
+			goto dll_deinit;
+	}
+
+	return 0;
+
+dll_deinit:
+	DeinitializeOls();
+
+	return -EFAULT;
+}
+
+void WinRing0_deinit(void)
+{
+	DeinitializeOls();
+}
