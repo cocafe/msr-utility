779584246dad8011441c1f3998280cc8c92da806
cocafe <cocafehj@gmail.com>
2018-06-03 11:56:36 +0800

    project: add wrmsr project, update properties
    Signed-off-by: cocafe <cocafehj@gmail.com>

---
+++ wrmsr/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <Windows.h>
+#include <windowsx.h>
+
+#include <getopt.h>
+
+int WINAPI WinMain(HINSTANCE hInstance,
+		   HINSTANCE hPrevInstance,
+		   LPSTR lpCmdLine,
+		   int nCmdShow)
+{
+	return 0;
+}
